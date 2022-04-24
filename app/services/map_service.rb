71bb897c462adc260db7e79e37c32b0618dcb21c
Eldridge Turambi <eturambi95@gmail.com>
2022-04-24 16:03:25 -0600

    Add: Methods for connection for coordinates

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -2,13 +2,20 @@ class MapService
   class << self
 
     def connection
-
+      Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/') do |faraday|
+        faraday.params['key'] = ENV['mapquest_key']
+      end
     end
 
-    def parse_json
-
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
     end
 
-    
+    def city_coordinates(city)
+      response = connection.get('address') do |faraday|
+        faraday.params['location'] = city
+      end
+      parse_json(response)
+    end
   end
 end
