b6cf3d4518a3e5947b51253f8e625727b9f04a72
Eldridge Turambi <eturambi95@gmail.com>
2022-04-25 11:53:23 -0600

    fix test for MunchieService

--- spec/services/munchies_service_spec.rb
+++ spec/services/munchies_service_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe MunchiesService do
   describe 'class methods' do
     it '.gets_restaurant_info', :vcr do
       food_response = MunchiesService.get_restaurant_info('chinese', 'new york city,ny')
+
       expect(food_response).to be_a(Hash)
       expect(food_response).to have_key(:businesses)
-      expect(food_response[:businesses][0]).to have_key(:categories)
+      expect(food_response[:businesses][0]).to have_key(:name)
+      expect(food_response[:businesses][0][:name]).to be_a(String)
+      expect(food_response[:businesses][0][:name]).to eq("Joe's Shanghai")
     end
   end
 end
