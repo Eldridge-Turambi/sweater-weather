2109a4ac3f85742104f024131d24c6e60849ab9a
Eldridge Turambi <eturambi95@gmail.com>
2022-04-24 22:37:22 -0600

    Add: configurations for vcr and webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,19 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('mapquest_key') { ENV['mapquest_key'] }
+  config.filter_sensitive_data('weather_key') { ENV['weather_key'] }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 1.hours }
+end
