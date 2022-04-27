97fd41b8c4519b5eda82bf2c3b63620ad44e9e81
Eldridge Turambi <eturambi95@gmail.com>
2022-04-27 00:47:38 -0600

    Add: Spec for road_trip_request for happy and sad path

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -15,10 +15,33 @@ RSpec.describe 'road trip request' do
     post '/api/v1/road_trip', params: params
 
     expect(response).to be_successful
-    expect(response.status).to eq(204)
+    expect(response.status).to eq(201)
 
     road_trip = JSON.parse(response.body, symbolize_names: true)
     expect(road_trip).to be_a(Hash)
+    expect(road_trip).to have_key(:data)
+    expect(road_trip[:data]).to have_key(:id)
+    expect(road_trip[:data]).to have_key(:type)
+    expect(road_trip[:data]).to have_key(:attributes)
+  end
 
+  it 'sad path for road trip request', :vcr do
+    user = User.create(email: 'randomperson@gmail.com', password: 'qwerty', password_confirmation: 'qwerty')
+
+    params = {
+      'origin': 'Denver,CO',
+      'destination': 'London,UK',
+      'api_key': user.api_key
+    }
+
+
+    post '/api/v1/road_trip', params: params
+
+
+    expect(response.status).to eq(400)
+
+    road_trip = JSON.parse(response.body, symbolize_names: true)
+    expect(road_trip).to be_a(Hash)
+    expect(road_trip[:message]).to eq('ooops Unable to find routes for these locations')
   end
 end
