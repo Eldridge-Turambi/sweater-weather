5d9189210c5bb69f071232e1a324d56575d9a2ff
Eldridge Turambi <eturambi95@gmail.com>
2022-04-27 00:49:36 -0600

    Update all controllers

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,10 +1,30 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
+    # start_location = params[:origin]
+    # end_location = params[:destination]
+    # roadtrip = RoadTripFacade.road_trip_data(start_location, end_location)
+    # render json: RoadTripSerializer.make_road_trip(start_location, end_location, roadtrip), status: 201
     start_location = params[:origin]
     end_location = params[:destination]
 
-    render json: RoadTripSerializer.make_road_trip(start_location, end_location, roadtrip), status: 201
+    user = User.find_by(api_key: params[:api_key])
+    trip_service = RoadTripService.road_trip_data(start_location, end_location)
+
+    # binding.pry
+
+
+    if user.present?
+      unless (trip_service[:info][:statuscode] == 402)
+        roadtrip = RoadTripFacade.road_trip_data(start_location, end_location)
+        render json: RoadTripSerializer.make_road_trip(start_location, end_location, roadtrip), status: 201
+      else
+        render json: {message: 'ooops Unable to find routes for these locations' }, status: 400
+      end
+    elsif user.present? == false
+      render json: {message: "Bad API key" }, status: 400
+    end
+
   end
 
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    if params[:password] == params[:password_confirmation]
-      user = User.create(user_params)
-      render json: UserSerializer.user_response(user)
+    # if params[:password] == params[:password_confirmation]
+    #   user = User.create(user_params)
+    #   render json: UserSerializer.user_response(user)
+    # else
+    #   render status: 404
+    # end
+    user = User.new(user_params)
+    if params[:password] != params[:password_confirmation]
+      render json: { status: 400, message: "Passwords Do Not Match" }
+    elsif User.find_by(email: user[:email]).present?
+      render json: { status: 400, message: "Email already exists" }
     else
-      render status: 404
+      user.save
+      render json: UserSerializer.user_response(user)
     end
   end
 
