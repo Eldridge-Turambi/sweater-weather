16dffe990548d7a5491c411acd44c82ba7a2f48f
Eldridge Turambi <eturambi95@gmail.com>
2022-04-27 00:49:03 -0600

    Add: road trip facade to include all needed attributes

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -3,8 +3,8 @@ class RoadTripFacade
 
     def road_trip_data(start_city, end_city)
       roadtrip_data = RoadTripService.road_trip_data(start_city, end_city)
-
-      RoadTrip.new(roadtrip_data)
+      end_city_weather = WeatherService.coordinate_weather(end_city)[:hourly]
+      RoadTrip.new(roadtrip_data, end_city_weather)
     end
   end
 end
